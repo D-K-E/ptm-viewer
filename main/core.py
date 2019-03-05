828ce3774647ac44fd2dfc8ab9709571486eb655
Kaan Eraslan <kaaneraslan@gmail.com>
2019-03-05 13:39:01 +0100

    'Notes and Parameters
    Here are the values of the parameters involved:
    
    light_source : {'direction': {'x': 2750.0, 'y': 2116.0, 'z': 20.0}, 'intensity': 1.0, 'ambient_coefficient': 0.0001, 'ambient_intensity': 1e-05}
    shader : {'diffuse_coeff': 0.8, 'spec_coeff': 0.1, 'shininess': 20.0, 'f_attr_c1': 1.0, 'f_attr_c2': 1.0, 'f_attr_c3': 0.0, 'diffuse_color_normalized': True, 'spec_color_normalized': True}
    output_name : t75.png
    '

--- main/core.py
+++ main/core.py
@@ -301,7 +301,7 @@ class ChannelShader:
                  color: np.ndarray,  # they are assumed to be O_d and O_s
                  spec_coeff=0.1,  # k_s
                  screen_gamma=2.2,
-                 diffuse_coeff=0.2,  # k_d
+                 diffuse_coeff=0.8,  # k_d
                  attenuation_c1=1.0,  # f_attr c1
                  attenuation_c2=1.0,  # f_attr c2 d_L coefficient
                  attenuation_c3=0.0,  # f_attr c3 d_L^2 coefficient
