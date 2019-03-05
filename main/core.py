0dc4e91e3fec066fb239c3c7437aa966fb8a16bb
Kaan Eraslan <kaaneraslan@gmail.com>
2019-03-05 14:21:15 +0100

    'Notes and Parameters
    Here are the values of the parameters involved:
    
    light_source : {'direction': {'x': 2750.0, 'y': 2116.0, 'z': 20.0}, 'intensity': 1.0, 'ambient_coefficient': 2e-09, 'ambient_intensity': 1.0}
    shader : {'diffuse_coeff': 0.1, 'spec_coeff': 1e-10, 'shininess': 2000.0, 'f_attr_c1': 1.0, 'f_attr_c2': 0.0, 'f_attr_c3': 0.0, 'diffuse_color_normalized': True, 'spec_color_normalized': 1.0}
    output_name : t100.png
    '

--- main/core.py
+++ main/core.py
@@ -305,8 +305,8 @@ class ChannelShader:
                  screen_gamma=2.2,
                  diffuse_coeff=0.1,  # k_d
                  attenuation_c1=1.0,  # f_attr c1
-                 attenuation_c2=1.0,  # f_attr c2 d_L coefficient
-                 attenuation_c3=1.0,  # f_attr c3 d_L^2 coefficient
+                 attenuation_c2=0.0,  # f_attr c2 d_L coefficient
+                 attenuation_c3=0.0,  # f_attr c3 d_L^2 coefficient
                  shininess=2000.0  # n
                  ):
         self.light_source = light_source
